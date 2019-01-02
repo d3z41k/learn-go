6c2b764797e671ef2c1c4fdbc7a8b1cd46491421
d3z41k <d3z41k@gmail.com>
2019-01-02 19:17:55 +0200

    Add async - rate limit

--- 05-async/08/ratelimit.go
+++ 05-async/08/ratelimit.go
@@ -4,33 +4,45 @@ import (
 	"sync"
 	"fmt"
 	"strings"
-	"runtime"
 	"time"
+	"runtime"
 )
 
 const (
 	iterationsNum = 7
 	goroutinesNum = 5
+	quotaLimit = 2
 )
 
-func startWorker(in int, wg *sync.WaitGroup) {
-	defer wg.Done() // уменьшаем счетчик на 1
-	for j := 0; j < iterationsNum; j++ {
+
+func startWorker(in int, wg *sync.WaitGroup, quotaCh chan struct{}) {
+	quotaCh <- struct{}{} // берем свободный слот
+	defer wg.Done()
+	for j :=0; j < iterationsNum; j++ {
 		fmt.Printf(formatWork(in, j))
+
+		if j%2 == 0 {
+			<-quotaCh  // возвращаем слот
+			quotaCh <- struct{}{} // берем слот
+		}
+
 		runtime.Gosched()
 	}
+	<-quotaCh // возвращаем слот
 }
 
 func main() {
 	wg := &sync.WaitGroup{} // инициализируем группу
+	quotaCh := make(chan struct{}, quotaLimit)
 	for i := 0; i < goroutinesNum; i++ {
 		wg.Add(1) // добавляем воркер
-		go startWorker(i, wg)
+		go startWorker(i, wg, quotaCh)
 	}
 	time.Sleep(time.Millisecond)
 
 	wg.Wait() // ожидаем, пока waiter.Done() не приведет счетчик к 0
 }
+
 
 func formatWork(in, j int) string {
 	return fmt.Sprintln(strings.Repeat("  ", in), "*",
