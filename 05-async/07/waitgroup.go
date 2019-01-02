6e1e726ed9a84680d8b875c991339b6a261d06f1
d3z41k <d3z41k@gmail.com>
2019-01-02 17:54:20 +0200

    Add async - wait group

---
+++ 05-async/07/waitgroup.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+	"strings"
+	"runtime"
+	"time"
+)
+
+const (
+	iterationsNum = 7
+	goroutinesNum = 5
+)
+
+func startWorker(in int, wg *sync.WaitGroup) {
+	defer wg.Done() // уменьшаем счетчик на 1
+	for j := 0; j < iterationsNum; j++ {
+		fmt.Printf(formatWork(in, j))
+		runtime.Gosched()
+	}
+}
+
+func main() {
+	wg := &sync.WaitGroup{} // инициализируем группу
+	for i:= 0; i < goroutinesNum; i++ {
+		wg.Add(1) // добавляем воркер
+		go startWorker(i, wg)
+	}
+	time.Sleep(time.Millisecond)
+
+	wg.Wait() // ожидаем, пока waiter.Done() не приведет счетчик к 0
+}
+
+func formatWork(in, j int) string {
+	return fmt.Sprintln(strings.Repeat("  ", in), "*",
+		strings.Repeat("  ", goroutinesNum-in),
+		"th", in,
+		"iter", j, strings.Repeat("*", j))
+}
